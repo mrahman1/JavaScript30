c5dd35345024bb817a02cce2a90dcff94ea698bf
github username <github email address>
2018-04-13 19:00:54 -0400

    added transiton event

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,24 +1,52 @@
 let keys = [
   {
-    note: "a",
-    code: "65",
+    note: "A",
     sound: "clap",
  },
  {
-   note: "s",
-   code: "83",
+   note: "S",
    sound: "hihat"
+ },
+ {
+   note: "D",
+   sound: "kick"
+ },
+ {
+   note: "F",
+   sound: "openhat"
+ },
+ {
+   note: "G",
+   sound: "boom"
+ },
+ {
+   note: "H",
+   sound: "ride"
+ },
+ {
+   note: "J",
+   sound: "snare"
+ },
+ {
+   note: "K",
+   sound: "tom"
+ },
+ {
+   note: "L",
+   sound: "tink"
  }
+
 ]
 
 for (let key of keys){
   let keyDiv = document.querySelector(".keys");
   let audioFile = `sounds/${key.sound}.wav`
+  let keyCode = key.note.charCodeAt()
 
   keyDiv.innerHTML += (
-    `<div data-key=${key.code} class="key">
+    `<div data-key=${keyCode} class="key">
       <kbd>${key.note}</kbd>
-      <audio data-key=${key.code} src="${audioFile}"></audio>
+      <audio data-key=${keyCode} src="${audioFile}"></audio>
     </div>`
   )
 }
@@ -27,8 +55,19 @@ for (let key of keys){
 function playSound(e){
   let pressedKey = event.which
   let audio = document.querySelector(`audio[data-key="${pressedKey}"]`);
-  console.log(audio)
+  let keyDiv = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  keyDiv.classList.add('playing');
   audio.play()
 }
+
+
+function removeTransition(e) {
+  e.target.classList.remove('playing');
+}
+
+const keyList = document.querySelectorAll('.key');
+keyList.forEach(key => key.addEventListener('transitionend', removeTransition));
+
 
 window.addEventListener('keydown', playSound)
